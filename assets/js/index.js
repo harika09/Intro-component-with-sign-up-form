b6a941091cc4664f95c5d849b78e8aa1d9a32395
Harold <harold.goldensky@gmail.com>
2020-12-14 18:42:01 +0800

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,50 +6,44 @@ function validate(){
     var lname = document.getElementById("lname").value;
     var email = document.getElementById("email").value;
     var password = document.getElementById("password").value;
-    var errorfname = document.getElementById("error-fname");
-    var errorlname = document.getElementById("error-lname");
-    var erroremail = document.getElementById("error-email");
-    var errorpassword = document.getElementById("error-password");
     var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
 
 
 
-    if(fname == ""){
-        form.classList.remove("valid");
-        form.classList.add("invalid");
-        document.getElementById("fname").style.borderColor = "red";
-        errorfname.innerHTML = ("First Name cannot be empty");
-        errorfname.style.color = "red";
-    }
-
-    if(lname == ""){
-        form.classList.remove("valid");
-        form.classList.add("invalid");
-        document.getElementById("lname").style.borderColor = "red";
-        errorlname.innerHTML = ("Last Name cannot be empty");
-        errorlname.style.color = "red";
-    }
-
-    if(password == ""){
-        form.classList.remove("valid");
-        form.classList.add("invalid");
-        document.getElementById("password").style.borderColor = "red";
-        errorpassword.innerHTML = ("Password cannot be empty");
-        errorpassword.style.color = "red";
+    if(fname == '' || lname == '' || password == '' || email == ''){
+       errorMessage();
     }
     
-
-    if(email.match(pattern)){
-        form.classList.add("valid");
-        form.classList.remove("invalid");
+    else if(email.match(pattern)){
+        success();
     }
 
     else{
-        form.classList.remove("valid");
-        form.classList.add("invalid");
-        document.getElementById("email").style.borderColor = "red";
-        erroremail.innerHTML = ("Looks like this is not an email");
-        erroremail.style.color = "red";
+       invalidEmailMessage();
        
     }
+}
+
+function errorMessage(){
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Fields cannot be empty',
+      })
+}
+
+function invalidEmailMessage(){
+    Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Email is Invalid!',
+      })
+}
+
+function success(){
+    Swal.fire(
+        'Good job!',
+        'This is just sample and not connected to the database',
+        'success'
+      )
 }
